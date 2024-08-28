1e61823e3b8e22326fba1afc56b4c688a8f1a983
Rayane <rayanebarros@outlook.com>
2024-08-28 15:19:24 -0300

    Acertado o caminho da api dos números

--- js/script.js
+++ js/script.js
@@ -50,6 +50,6 @@ const funcionamento = new Funcionamento("[data-semana]", "aberto");
 // console.log(funcionamento);
 funcionamento.init();
 
-fetchAnimais("../../animaisapi.json", ".numeros-grid");
+fetchAnimais("./animaisapi.json", ".numeros-grid");
 
 fetchBitcoin("https://blockchain.info/ticker", ".btc-preco");
